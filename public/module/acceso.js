e6443887fad3cbf8baf726becbe5a489bf4f1641
fbarbera <34081993+fbarbera@users.noreply.github.com>
2018-01-25 20:09:26 -0300

    New modules
    Add module usuario, puerta, acceso

---
+++ public/module/acceso.js
@@ -0,0 +1,28 @@
+function acceso()
+{
+    var usuario = document.getElementById('id_usuario').value;
+    var pass = document.getElementById('id_pass').value;
+    var puerta = document.getElementById('id_puerta'.value);
+
+
+    var datos = new Object();
+	datos.id_usuario = usuario;
+	datos.id_pass = pass;
+    datos.id_puerta = puerta;
+    
+    function crearAcceso(acceso){    
+        
+    //creaacceso
+
+    }
+
+    function updateAcceso(acceso){
+
+    //UpdateUser
+    }
+
+    function deleteAcceso(acceso){
+        //deleteuser
+    }
+
+}
---
+++ public/module/puerta.js
@@ -0,0 +1,19 @@
+function puerta()
+{
+    var puerta = document.getElementById('id_puerta');
+    function crearPuerta(puerta){    
+        
+    //creapuerta
+
+    }
+
+    function updatePuerta(puerta){
+
+    //UpdateUser
+    }
+
+    function deletePuerta(puerta){
+        //deleteuser
+    }
+
+}
