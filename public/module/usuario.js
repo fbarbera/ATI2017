37ec58764a9008f92ef63f0e34474a5a539b5ddd
fbarbera <34081993+fbarbera@users.noreply.github.com>
2018-01-27 19:28:13 -0300

    abm usuarios

--- public/module/usuario.js
+++ public/module/usuario.js
@@ -1,11 +1,19 @@
-function usuario()
+function abmUsuario(accion)
 {
+   
+    alert(accion);
+    switch(accion) {
+        case 1:
+        crearUsuario();
+            break;
+        case 2:
+        updateUsuario();
+            break;
+        case 3:
+        deleteUsuario();
+            break;
+    }
     var Ausuario = document.getElementById('inputUsuario').value;
-
-
-    alert('llego' + Ausuario);
-
-
 
    function crearUsuario(usuario){    
         
